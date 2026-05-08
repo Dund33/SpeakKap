d6a85981975f7b938740d72889fc703592bf0295
Luke <warior1112@gmail.com>
2026-05-08 23:15:01 +0200

    Made the tests more restrictive and added some more comments

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -25,10 +25,12 @@ def test_register(client, data_partitions):
         files=files,
     )
 
+    #Response should be either 201 Created (if registration is successful) or 409 Conflict (if user already exists)
     assert response.status_code in [201, 409]
 
     json_data = response.json()
 
+    #Response should contain valid JSON data
     assert json_data is not None
 
 
@@ -48,8 +50,10 @@ def test_identify(client, data_partitions):
         )
         responses.append(response)
 
-    assert all(response.status_code in [200, 404] for response in responses)
+    #Responses should be 200 OK
+    assert all(response.status_code == 200 for response in responses)
 
+    #All responses should contain valid JSON data
     assert all(map(lambda r: r.json() is not None, responses))
 
     for response in responses:
